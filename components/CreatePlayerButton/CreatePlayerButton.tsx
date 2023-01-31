6117e813f700b9a84b81fbf83d2023c6bcfd3ccb
Sweets Sweetman <sweetmantech@gmail.com>
2023-01-31 11:19:10 -0300

    I toast a valid player, with new IPFS playlist on button click.

--- components/CreatePlayerButton/CreatePlayerButton.tsx
+++ components/CreatePlayerButton/CreatePlayerButton.tsx
@@ -2,6 +2,9 @@ import { useNetwork, useSigner } from "wagmi";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
 import styles from "../../styles/Home.module.css";
 import { useState } from "react";
+import { toast } from "react-toastify";
+import { ipfs } from "@decent.xyz/sdk";
+import { NFTStorage } from "nft.storage";
 
 const CreatePlayerButton = (props: any) => {
   const { contractAddress } = props;
@@ -18,9 +21,75 @@ const CreatePlayerButton = (props: any) => {
     const recipients = [contractAddress];
     setLoading(true);
     try {
+      const file = new File(["foo"], "foo.png", {
+        type: "image/png",
+      });
+
+      const client = new NFTStorage({
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDU0NUY3MmE2RTE4ZTc1REZBMTA3Qjc3REIzNDM1NDNjOTQzMEI0RmQiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2Mjc1MDI4MjU5NiwibmFtZSI6IkRFQ0VOVCJ9.KaoP8CYmUESkkDo5XoCMEomfQBZiK7E_hpkMUX8uHFY",
+      });
+
+      const jsonString = JSON.stringify({
+        schemaVersion: "1",
+        coverArtUrl:
+          "https://cdn.warpsound.ai/ipfs/QmVzZenmmA1zm47dsto7gqmxSZPMNZF6hqy3Jesmb24UDQ/WS-Nayomi-Album-BestOf2022-FINAL.webp",
+        title: "the record store_best of (2022)",
+        artist: "Nayomi",
+        chain: "Ethereum",
+        contractAddress: "0xeacf3bc37a3bf1c1166a0d9a4df3f1679c26b52e",
+        tokenId: "1",
+        tokenType: "ERC-1155",
+        hideBranding: false,
+        items: [
+          {
+            kind: "audio",
+            title: "Lonely",
+            artist: "X&ND",
+            url: "https://maroon-quickest-ocelot-736.mypinata.cloud/ipfs/QmfQRWph2EGZi6CnLF3w2DWGt57FHRYxXKTBDVUiGFhJhy/LONELY_X_ND--online-audio-convert.com.mp3",
+          },
+          {
+            kind: "audio",
+            title: "Beg For It (Explicit)",
+            artist: "X&ND",
+            url: "https://maroon-quickest-ocelot-736.mypinata.cloud/ipfs/QmfQRWph2EGZi6CnLF3w2DWGt57FHRYxXKTBDVUiGFhJhy/BEG%20FOR%20IT%28EXPLICIT%29_X_ND--online-audio-convert.com.mp3",
+          },
+          {
+            kind: "audio",
+            title: "Can You Let Me In? (feat Melody Wagner)",
+            artist: "X&ND",
+            url: "https://maroon-quickest-ocelot-736.mypinata.cloud/ipfs/QmfQRWph2EGZi6CnLF3w2DWGt57FHRYxXKTBDVUiGFhJhy/CAN%20YOU%20LET%20ME%20IN_X_ND_Melodie%20Wagner--online-audio-convert.com.mp3",
+          },
+        ],
+      });
+
+      const bytes = new TextEncoder().encode(jsonString);
+      const blob = new Blob([bytes], {
+        type: "application/json;charset=utf-8",
+      });
+
+      const ipfsResponse = (await client.storeBlob(blob)) as any;
+
+      console.log("ipfsResponse", ipfsResponse);
+      if (ipfsResponse?.error) throw ipfsResponse?.error;
       console.log(
-        "TODO: USE DECENT SDK TO CREATE IPFS METADATA IN PLAYLIST JSON FORMAT"
+        "TODO: DECENT SDK TO CREATE IPFS METADATA IN PLAYLIST JSON FORMAT"
       );
+
+      const baseAnimationUrl =
+        "https://cdn.warpsound.ai/ipfs/QmVYW5vHaV322Kvp2So5ErngP1PrDUneYqo4e9TNygAGSn?playlist-url=";
+      const playlistUrl = `https://nftstorage.link/ipfs/${ipfsResponse}`;
+      toast.success(
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={baseAnimationUrl + playlistUrl}
+        >
+          View your player here
+        </a>,
+        { autoClose: false }
+      );
+
       console.log("TODO: TOAST / REDIRECT to PLAYER? SHARE ON LENS BUTTON");
     } catch (err) {
       console.error(err);
--- components/PlayerCreatePage/PlayerCreatePage.tsx
+++ components/PlayerCreatePage/PlayerCreatePage.tsx
@@ -1,13 +1,9 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import type { NextPage } from "next";
 import { useForm, FormProvider } from "react-hook-form";
 import styles from "../../styles/Home.module.css";
-import Image from "next/image";
-import Link from "next/link";
 import SeoHead from "../SeoHead";
-import ImageUpload from "../MediaUpload/ImageUpload";
-import AudioUpload from "../MediaUpload/AudioUpload";
-import CreatePlayerButton from "../CreatePlayerButton";
+import PlayerCreateForm from "../PlayerCreateForm";
+import Footer from "../Footer";
 
 const PlayerCreatePage: NextPage = () => {
   const methods = useForm();
@@ -16,78 +12,8 @@ const PlayerCreatePage: NextPage = () => {
     <FormProvider {...methods}>
       <div className={`${styles.container} background`}>
         <SeoHead />
-        <main className={styles.main}>
-          <div className="flex items-center gap-4">
-            <ConnectButton />
-            <Link
-              href="https://github.com/SweetmanTech/kpi-dashboard"
-              target="_blank"
-            >
-              <Image
-                src="/images/github-mark-white.svg"
-                height={22}
-                width={22}
-                alt="link to repository"
-              />
-            </Link>
-          </div>
-
-          <h1 className={`${styles.title} font-medium`}>Player Creator</h1>
-          <div className="flex gap-10">
-            <div className="flex flex-col items-center">
-              <p className="pb-2 font-medium">Project Name</p>
-
-              <input
-                className={`input-text text-black rounded-full p-4`}
-                placeholder="X&NDYLAND"
-                onChange={console.log}
-              />
-            </div>
-            <div className="flex flex-col items-center">
-              <p className="pb-2 font-medium">Artist Name</p>
-
-              <input
-                className={`input-text text-black rounded-full p-4`}
-                placeholder="X&ND"
-                onChange={console.log}
-              />
-            </div>
-          </div>
-          <ImageUpload
-            nftImage={{ preview: "/icons/img-placeholder.png" }}
-            setNftImage={console.log}
-            formRegisterName="formRegisterName"
-            label="cover art"
-          />
-          <AudioUpload
-            audioFile={{ preview: "/icons/audio-placeholder.png" }}
-            setAudioFile={console.log}
-            formRegisterName="formRegisterName"
-          />
-          <CreatePlayerButton />
-        </main>
-
-        <footer className="py-8 border-t border-white text-white">
-          <div>
-            <p className="flex justify-center pb-4 text-xl">
-              for the musicians 💿
-            </p>
-            <a
-              className="flex justify-center items-center text-xl"
-              href="https://decent.xyz"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="pr-4">🏗️</span>
-              <Image
-                src="/images/decent.png"
-                height={18}
-                width={100}
-                alt="Decent 💪"
-              />
-            </a>
-          </div>
-        </footer>
+        <PlayerCreateForm />
+        <Footer />
       </div>
     </FormProvider>
   );
